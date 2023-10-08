75fb35eff59abc25e29940eb247db286fcd890ae
barisbalcikoca1 <86593147+barisbalcikoca1@users.noreply.github.com>
2023-10-08 19:35:30 +0300

    Odev1
    Degisken Olusturma
    Sabit Olusturma
    Tur Donusumu
    Odev1

---
+++ app/src/main/java/com/barisbalcikoca/javadersleri/degiskenler/Odev1.java
@@ -0,0 +1,62 @@
+package com.barisbalcikoca.javadersleri.degiskenler;
+
+public class Odev1 {
+    public static void main(String[] args) {
+        String ilce = "Odunpazarı";
+        String kita = "Asya";
+        String faks = "+90 222 220 42 36";
+        int postaKodu = 26250;
+        String instaAdresi = "barisbalcikoca";
+        String calistiginBolum = "AR-GE";
+        int urunMiktarı = 250;
+        String dogumTarihi = "26.09.1999";
+        double borc = 980.75;
+        String medeniHal = "Bekar";
+        String videoYorumu = "Harika bir deneyim";
+        String odemeSaati = "15:42";
+        float eftMiktari = 1500.000f;
+        int satilanMiktar = 8;
+        String telefonModeli = "Xiaomi";
+        String dergiAdi = "Uykusuz";
+        String basimTarihi = "08.10.2023";
+        int zamMiktari = 15;
+        int daireSayisi = 8;
+        double enlem =26.45;
+        double boylam = 36.42;
+        String yemekAdi = "Pilav";
+        int urunFiyati = 98;
+        String sirket = "Formatçılar";
+        String videoAdi = "Android Bootcamp Programı | Java | 2023";
+        double muzikSuresi =38.58;
+        int mekanPuani = 4;
+        String dosyaAdi = "JavaDersleri";
+        String resimFormati = "jpg";
+        String renk = "Beyaz";
+        String renkKodu = "#FFFFFF";
+        String bilgisayarModeli = "Casper-Excalibur";
+        int ekranBoyutu =24;
+        double kullanimSuresi = 5.29;
+        double basinc = 10.0;
+        String etkinlikGunu = "20 Ocak Perşembe";
+        String odemeGunu = "Pazartesi";
+        String yolculukCikisTarihi = "26.09.1999";
+        String mahalleAdi = "Emek";
+        String otobusHatti = "Odunpazarı-ŞehirHastanesi";
+        float kullanilanDakika =38.45f;
+        long kargoTakipNo = 26262626262626L;
+        int kuponSuresi = 5;
+        String kuponKodu = "indirim5";
+        String faturaTarihi = "8.10.2023";
+
+
+        System.out.println("İlçe : " + ilce);
+        System.out.println("Kıta : " + kita);
+        System.out.println("Faks No : " + faks);
+        System.out.println("Posta Kodu : " + postaKodu);
+        System.out.println("İnstagram Adresi : " + instaAdresi);
+        System.out.println("Çalıştığı Bölüm : " + calistiginBolum);
+        System.out.println("ÜrÜn Miktari : " + urunMiktarı);
+        System.out.println("Doğum Tarihi : " + dogumTarihi);
+
+    }
+}
