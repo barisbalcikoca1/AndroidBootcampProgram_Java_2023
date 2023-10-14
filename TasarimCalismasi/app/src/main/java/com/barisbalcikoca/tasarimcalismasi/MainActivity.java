c7b0c01579cc3073e7bfc1d69a8bc42f0299cd22
barisbalcikoca1 <86593147+barisbalcikoca1@users.noreply.github.com>
2023-10-14 17:41:58 +0300

    TasarimCalismasi
    StatusBarColor-Toolbar
    
    ConstraintLayout- Sabitleme- Align-Chain- Center- String Dosyası
    
    Çoklu Dil&Ekran Desteği- Uygulama İconu- Dark Mode

---
+++ TasarimCalismasi/app/src/main/java/com/barisbalcikoca/tasarimcalismasi/MainActivity.java
@@ -0,0 +1,18 @@
+package com.barisbalcikoca.tasarimcalismasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //materialDesign --> renkler için
+        //uplabs.com    --> tasarım esinlenmek için
+        //adobecolor
+    }
+}
