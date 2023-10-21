a1a2474d4b02ef0e51ce311732bbc24769a14753
barisbalcikoca1 <86593147+barisbalcikoca1@users.noreply.github.com>
2023-10-22 00:17:43 +0300

    Odev5 - Hesap Makinesi (Calculator)

---
+++ Odev5/app/src/main/java/com/barisbalcikoca/odev5/MainActivity.java
@@ -0,0 +1,78 @@
+package com.barisbalcikoca.odev5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.barisbalcikoca.odev5.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private String sonuc = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.tvSonuc.setText(sonuc);
+
+        binding.btn0.setOnClickListener(view -> {
+            binding.tvSonuc.setText(binding.tvSonuc.getText()+"0");
+        });
+        binding.btn1.setOnClickListener(view -> {
+            binding.tvSonuc.setText(binding.tvSonuc.getText()+"1");
+        });
+        binding.btn2.setOnClickListener(view -> {
+            binding.tvSonuc.setText(binding.tvSonuc.getText()+"2");
+        });
+        binding.btn3.setOnClickListener(view -> {
+            binding.tvSonuc.setText(binding.tvSonuc.getText()+"3");
+        });
+        binding.btn4.setOnClickListener(view -> {
+            binding.tvSonuc.setText(binding.tvSonuc.getText()+"4");
+        });
+        binding.btn5.setOnClickListener(view -> {
+            binding.tvSonuc.setText(binding.tvSonuc.getText()+"5");
+        });
+        binding.btn6.setOnClickListener(view -> {
+            binding.tvSonuc.setText(binding.tvSonuc.getText()+"6");
+        });
+        binding.btn7.setOnClickListener(view -> {
+            binding.tvSonuc.setText(binding.tvSonuc.getText()+"7");
+        });
+        binding.btn8.setOnClickListener(view -> {
+            binding.tvSonuc.setText(binding.tvSonuc.getText()+"8");
+        });
+        binding.btn9.setOnClickListener(view -> {
+            binding.tvSonuc.setText(binding.tvSonuc.getText()+"9");
+        });
+        binding.btnClear.setOnClickListener(view -> {
+            binding.tvSonuc.setText("");
+        });
+
+        binding.btnPlus.setOnClickListener(view -> {
+            binding.tvSonuc.setText(binding.tvSonuc.getText()+"+");
+        });
+
+        binding.btnEsittir.setOnClickListener(view -> {
+            sonuc = binding.tvSonuc.getText().toString(); // Sonucu alın ve String türüne çevirin
+            String[] sonDeger = sonuc.split("\\+");
+            int toplam = 0;
+            for (String deger : sonDeger) {
+                if (!deger.isEmpty()) { // Boş dizeleri atlayın
+                    toplam += Integer.parseInt(deger);
+                }
+            }
+            binding.tvSonuc.setText(String.valueOf(toplam)); // Sonucun metin olarak ekrana yazılması
+        });
+
+
+    }
+
+}
